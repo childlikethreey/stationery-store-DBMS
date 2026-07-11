d49114271b9498ba7ebcf31a8b4772bf96b39edf
Your Name <carol1998605@gmail.com>
2026-07-11 21:04:23 +0800

    staff debug, create premission

--- routes/staff.py
+++ routes/staff.py
@@ -1,10 +1,12 @@
 import mysql.connector
 from flask import Flask, Blueprint, jsonify, request
 from db import get_connection
+from routes.auth_required import admin_required
 
 staff_bp = Blueprint("staff", __name__)
 
 @staff_bp.route("/staff", methods = ["get"])
+@admin_required
 def show_all():
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
@@ -22,6 +24,7 @@ def show_all():
         conn.close()
 
 @staff_bp.route("/staff", methods = ["post"])
+@admin_required
 def create_staff():
     data = request.get_json()
     name = data.get["name"]
@@ -53,6 +56,7 @@ def create_staff():
         conn.close()
 
 @staff_bp.route("/staff/<int:staff_id>", methods = ["put"])
+@admin_required
 def update_staff(staff_id):
     data = request.get_json(silent=True) or {}
     col = []
